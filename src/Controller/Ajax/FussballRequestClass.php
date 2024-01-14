a2f30fa06b9d95718ca1782ec16be3955bf2da6d
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2024-01-14 11:39:31 +0100

    Update FussballRequestClass.php

--- src/Controller/Ajax/FussballRequestClass.php
+++ src/Controller/Ajax/FussballRequestClass.php
@@ -2370,7 +2370,7 @@ $debug.=" sql: $sql\n";
 	    $sql = "UPDATE hy_teilnehmer $value where ID='$id'";
         $debug.="UPDATE Teilnehmer sql: $sql<br>";	
         $cnt = $this->connection->executeStatement($sql);
-	    $html.="Teilnehmer " . $_GET['Kurzname'] . " ge&auml;ndert";
+	    $html.="Teilnehmer " . $Kurzname . " ge&auml;ndert";
       }
       $html = utf8_encode($html);
       return new JsonResponse(['data' => $html,'error'=>$errortxt, 'debug'=>$debug]); 
