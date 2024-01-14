9ab42d6439304dffc6e9a7c0cd6de93628e0b87a
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2024-01-14 16:00:45 +0100

    bereinigung

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,4 +1,4 @@
 <?php
-$GLOBALS['TL_CSS'][] = 'bundles/ajaxarticle/assets/css/modal.css';
-$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/ajaxarticle/assets/js/ajaxhelper.js';
+//$GLOBALS['TL_CSS'][] = 'bundles/ajaxarticle/assets/css/modal.css';
+//$GLOBALS['TL_JAVASCRIPT'][] = 'bundles/ajaxarticle/assets/js/ajaxhelper.js';
         
